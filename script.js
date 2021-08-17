009d23b9292f05799339dcd39d32af9b92831f29
Jay For Short <jagoblacksky@gmail.com>
2021-08-17 02:51:12 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+// Todo: Write the code for lesson 79...
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsShowModal = document.querySelectorAll(".show-modal");
+console.log(btnsShowModal);
+const openModal = function () {
+  console.log("Button clicked, Window open");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = function () {
+  console.log("Modal closed");
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape") {
+    console.log("Esc was pressed");
+    if (!modal.classList.contains("hidden")) {
+      closeModal();
+    }
+  }
+});
