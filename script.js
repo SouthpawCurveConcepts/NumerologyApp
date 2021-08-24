00ceb5d01691952d16719683d8e0de343ccba1f5
Jay For Short <jagoblacksky@gmail.com>
2021-08-23 22:50:44 -0500

    3.  Multiple modal windows and buttons inprogress

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
-"use strict";
+'use strict';
 // Todo: Write the code for lesson 79...
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnsShowModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log(btnsShowModal);
 const openModal = function () {
-  console.log("Button clicked, Window open");
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+  console.log('Button clicked, Window open');
+  console.log(typeof btnsShowModal);
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 const closeModal = function () {
-  console.log("Modal closed");
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+  console.log('Modal closed');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 for (let i = 0; i < btnsShowModal.length; i++)
-  btnsShowModal[i].addEventListener("click", openModal);
-btnCloseModal.addEventListener("click", closeModal);
+  btnsShowModal[i].addEventListener('click', openModal);
 
-overlay.addEventListener("click", closeModal);
+btnCloseModal.addEventListener('click', closeModal);
 
-document.addEventListener("keydown", function (e) {
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
   console.log(e.key);
 
-  if (e.key === "Escape") {
-    console.log("Esc was pressed");
-    if (!modal.classList.contains("hidden")) {
+  if (e.key === 'Escape') {
+    console.log('Esc was pressed');
+    if (!modal.classList.contains('hidden')) {
       closeModal();
     }
   }
